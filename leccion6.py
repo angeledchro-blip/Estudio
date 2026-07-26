f9693262071759371b562ea890505a92913a4943
angeledchro-blip <mundobago123@gmail.com>
2026-07-26 00:00:07 -0700

    Leccion 6: return, con repaso p6

---
+++ leccion6.py
@@ -0,0 +1,4 @@
+def pordos(numero):
+    return numero * 2
+resultado = pordos(5)
+print(resultado)
---
+++ p6.py
@@ -0,0 +1,5 @@
+def triple(numero):
+    return numero * 3
+resultado = triple(2)    
+resultado = resultado + 1 
+print(resultado)
