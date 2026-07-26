45bc4a2ce047f710af36b539aa0d64d63be5d4f1
angeledchro-blip <mundobago123@gmail.com>
2026-07-25 21:42:47 -0700

    Ajuste menor en leccion1 y reescritura de p4v2 (repaso reforzado)

--- leccion1.py
+++ leccion1.py
@@ -2,4 +2,5 @@ nombre = "Angel"
 edad = 22
 
 print("Me llamo", nombre, "y tengo", edad, "años.")
-print(f"ME llamo {nombre} y Tengo {edad} Años.")
+print(f"Me llamo {nombre} y Tengo {edad} Años.")
+
--- p4v2.py
+++ p4v2.py
@@ -1,26 +1,21 @@
-personas = ["Ana", "Luis", "Sofia"]
-
-for persona in personas:
-
+nombres = ["Ezra", "Diego", "Edmundo"]
+for nombre in nombres:
     while True:
-        try:
-            horas = int(input(f"{persona}, cuantas horas dormiste? "))
-
-            if horas < 0:
-                print("Coloca un numero mayor a 0")
-                continue
-            elif horas > 24:
-                print("Coloca un numero por debajo de 24")
-                continue
-        except ValueError:
-            print("Coloca exclusivamente numeros")
-            continue
-
-        break
-
-    if horas < 6:
-        print(f"{persona} durmio poco")
-    elif horas <= 9:
-        print(f"{persona} durmio lo adecuado")
-    elif horas > 9:
-        print(f"{persona} durmio de mas")
+     try:
+        horas = int(input(f"{nombre} Cuantas horas dormiste?"))
+        if horas <0:
+         print("Coloca un numero mayor a 0")
+         continue
+        elif horas >24:
+         print("Coloca un numero menor a 24") 
+         continue
+     except ValueError:
+         print("Coloca un numero valido")   
+         continue
+     break   
+    if horas <6:
+     print(f"{nombre} Dormiste poco")
+    elif horas <9:
+     print(f"{nombre} Dormiste lo adecuado")
+    else:
+     print(f"{nombre} Dormiste de mas")      
