edc38371b907313227766a73cb6d33680812e28f
angeledchro-blip <mundobago123@gmail.com>
2026-07-25 20:07:20 -0700

    Leccion 4: listas y bucles for, con repaso p4

---
+++ leccion4.py
@@ -0,0 +1,7 @@
+numeros = [3, 5, 2, 22, 9]
+for numero in numeros:
+ print(numero)
+ if numero % 2 == 0:
+  print("Es par")
+ else:
+  print("Es impar") 
---
+++ p4.py
@@ -0,0 +1,8 @@
+edades = [18, 15, 20, 25, 30]
+for edad in edades:
+    print(edad)
+    if edad <18:
+        print("Eres menor de edad")
+    else:
+        print("Eres mayor de edad")    
+    
