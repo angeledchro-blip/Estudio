44e9c8ba7d363d403c7f1885891737095d3b754a
angeledchro-blip <mundobago123@gmail.com>
2026-07-25 22:22:09 -0700

    Leccion 5: funciones con parametros, con repaso p5

---
+++ leccion5.py
@@ -0,0 +1,8 @@
+def es_par(numero):
+    if numero % 2 == 0:
+        print("Es par")
+    else:
+        print("Es impar")    
+es_par(4)
+es_par(7)
+es_par(10)        
---
+++ p5.py
@@ -0,0 +1,12 @@
+def calisificar_edad(edad):
+    print(edad)
+    if edad <12:
+        print("Niño")
+    elif edad <=17:
+        print("Adolescente")    
+    else:
+        print("Adulto") 
+calisificar_edad(11)
+calisificar_edad(17)   
+calisificar_edad(15)
+calisificar_edad(19)      
